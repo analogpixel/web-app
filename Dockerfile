2a819ff101bc2eea6aae1830143346538bc5ced9
analogpixel <matt.poepping@gmail.com>
2021-10-26 09:43:28 -0600

    fp

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM tiangolo/uwsgi-nginx-flask:python3.8
+#FROM tiangolo/meinheld-gunicorn-flask:python3.9
+
+ENV FLASK_ENV=development
+ENV FLASK_APP=main.py
+
+COPY ./app /app
+COPY "entrypoint.sh" "/entrypoint.sh"
+RUN chmod 755 /entrypoint.sh
+COPY "envconsul" "/bin/envconsul"
+
+#ENTRYPOINT ["/entrypoint.sh"]
+#CMD ["/bin/envconsul", "-consul-addr","consul-ui.default.svc.cluster.local","--prefix=sample-app","/start.sh"]
+#CMD ["/start.sh"]
---
+++ app/main.py
@@ -0,0 +1,19 @@
+from flask import Flask, Blueprint
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "hello World:" + str(os.environ.get('app_config_1'))
+    #return app.send_static_file('index.html')
+
+@app.route('/topic')
+def get_topic():
+    # https://kafka-python.readthedocs.io/en/master/usage.html
+    print("topics")
+
+if __name__ == "__main__":
+    # Only for debugging while developing
+    app.run(host='0.0.0.0', debug=True, port=80)
+
